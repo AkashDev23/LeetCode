ac25cb75ee3c95f1987bef74a9bb435de4526415
Akash Dev <107734937+AkashDev23@users.noreply.github.com>
2023-07-12 11:38:53 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (49.97%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        //In this question we will use the floyd's cycle detection algorithm
+        //we will take two pointers fast and slow and while they are not null we will move fast pointer by 2 step and slow pointer by 1 step
+        //If at any place both the fast and slow pointer meet then our linkedlist contains a cycle.
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
