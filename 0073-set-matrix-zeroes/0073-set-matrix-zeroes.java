47d57cabe9b0a4aee01a81719a0b2cf9de12a11c
Akash Dev <107734937+AkashDev23@users.noreply.github.com>
2024-02-29 08:03:19 +0530

    Time: 1 ms (76.11%), Space: 44.9 MB (98.99%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,21 +1,24 @@
 class Solution {
-    public void setZeroes(int[][] arr) {
-        int m[][]=new int [arr.length][arr[0].length];
-    for(int i=0;i<arr.length;i++){
-        for(int j=0;j<arr[0].length;j++){
-            if(arr[i][j]==0)
-            {
-                for(int k=0;k<arr[0].length;k++)
-                m[i][k]=1;
-                for(int k=0;k<arr.length;k++)
-                m[k][j]=1;
+    public void setZeroes(int[][] matrix) {
+        //First make a new matrix of the same length as the original matrix and set all the rows and columns of where the zero occurs to 1.(You could choose any number I have choosed 1.
+        int arr[][]=new int[matrix.length][matrix[0].length];
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(matrix[i][j]==0){
+                    for(int k=0; k<matrix.length; k++){
+                        arr[k][j]=1;
+                    }
+                    for(int k=0; k<matrix[0].length; k++){
+                        arr[i][k]=1;              }
+                }
             }
         }
-    }
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[0].length;j++){
-              if(m[i][j]==1)
-              arr[i][j]=0;
+        //Now for any element in this new matrix is 1 set those elements of the original matrix to 0
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(arr[i][j]==1){
+                    matrix[i][j]=0;
+                }
             }
         }
     }
